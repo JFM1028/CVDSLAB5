2cb5c43b0fb9640a97975e5223e9de707c55991d
Diego <58009401+marzo245@users.noreply.github.com>
2024-10-19 14:21:34 -0500

    correccion en repository

--- GestorTareas/GestorTareasBackend/gestortareasbackend/gestortareasbackend/src/main/java/com/hazinlab/gestortareasbackend/repository/TareaRepository.java
+++ GestorTareas/GestorTareasBackend/gestortareasbackend/gestortareasbackend/src/main/java/com/hazinlab/gestortareasbackend/repository/TareaRepository.java
@@ -3,13 +3,12 @@ package com.hazinlab.gestortareasbackend.repository;
 import com.hazinlab.gestortareasbackend.model.Tarea;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
+
 /**
  * Interfaz que define el repositorio para la entidad Tarea.
  * Esta interfaz hereda de MongoRepository, lo que proporciona métodos
  * CRUD (Crear, Leer, Actualizar, Eliminar) para la entidad Tarea.
  */
 public interface TareaRepository extends MongoRepository<Tarea, String> {
-    // No se requieren métodos adicionales, ya que MongoRepository proporciona
-    // la funcionalidad necesaria para manejar tareas en la base de datos MongoDB.
-    list<Tarea> findByUsuarioId(String usuarioId);
+    List<Tarea> findByUsuarioId(String usuarioId);
 }
